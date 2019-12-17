c0e5462a968e6ab9f2a6b6cc0f49206250becbf9
Matthew McAteer <matthewmcateer0@gmail.com>
2019-12-16 17:48:37 -0800

    Added `_time_distributed_dense()` function

--- attention_decoder.py
+++ attention_decoder.py
@@ -5,10 +5,41 @@ https://machinelearningmastery.com/encoder-decoder-attention-sequence-to-sequenc
 import tensorflow as tf
 from keras import backend as K
 from keras import regularizers, constraints, initializers, activations
-from keras.layers.recurrent import Recurrent, _time_distributed_dense
+from keras.layers.recurrent import Recurrent#, _time_distributed_dense
 from keras.engine import InputSpec
 
 tfPrint = lambda d, T: tf.Print(input_=T, data=[T, tf.shape(T)], message=d)
+
+def _time_distributed_dense(x, w, b=None, dropout=None,
+                           input_dim=None, output_dim=None, timesteps=None):
+    '''Apply y.w + b for every temporal slice y of x.
+    '''
+    if not input_dim:
+        # won't work with TensorFlow
+        input_dim = K.shape(x)[2]
+    if not timesteps:
+        # won't work with TensorFlow
+        timesteps = K.shape(x)[1]
+    if not output_dim:
+        # won't work with TensorFlow
+        output_dim = K.shape(w)[1]
+
+    if dropout:
+        # apply the same dropout pattern at every timestep
+        ones = K.ones_like(K.reshape(x[:, 0, :], (-1, input_dim)))
+        dropout_matrix = K.dropout(ones, dropout)
+        expanded_dropout_matrix = K.repeat(dropout_matrix, timesteps)
+        x *= expanded_dropout_matrix
+
+    # collapse time dimension and batch dimension together
+    x = K.reshape(x, (-1, input_dim))
+
+    x = K.dot(x, w)
+    if b:
+        x = x + b
+    # reshape to 3D tensor
+    x = K.reshape(x, (-1, timesteps, output_dim))
+    return x
 
 class AttentionDecoder(Recurrent):
 
